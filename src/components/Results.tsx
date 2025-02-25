5c8cbd3ab0af733e5d03313596e87636518475c9
Rafael Paragon <rvirtualmark2@gmail.com>
2025-02-25 23:01:38 +0000

    v1 ajuste 0.1

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -47,7 +47,7 @@ const Results = () => {
       ],
       // Imagem (16:9) - Sugestão de dimensão: ~1200x675px
       // Substitua por um path real no seu projeto ou um placeholder
-      image: '/public/game-safari.png'
+      image: '/game-safari.png'
     },
     {
       // 2) Amazônia Vital
@@ -64,7 +64,7 @@ const Results = () => {
         { value: '+95%', label: 'Aumento na Taxa de Retorno' }
       ],
       // Sugestão de dimensão: ~1200x675px
-      image: '/public/amazonia-vital.png'
+      image: '/amazonia-vital.png'
     },
     {
       // 3) Colonial Guararema
